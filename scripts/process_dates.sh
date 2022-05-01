db7bc9c1bf2b22051949e9796426b9f94f5074b2
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2022-05-01 06:31:23 +0100

    process multiple dates

---
+++ scripts/process_dates.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+while read line; do
+  echo standard $line
+  Rscript scripts/estimate.R -d 1 -g -a -m $line
+done < scripts/dates
+
+while read line; do
+  echo higher $line
+  Rscript scripts/estimate.R -d 1 -g -a -i -m $line
+done < scripts/dates
